011f9b672cfd827520b8ec4c6eedd375e0e7d7ad
Lionel Henry <lionel.hry@gmail.com>
2014-10-08 09:40:38 +0200

    some cleanups

--- R/object-matrix.r
+++ R/object-matrix.r
@@ -1,67 +1,5 @@
 
-
-## ## todo: named arguments
-## cbind.data <- function(...) {
-##   dots <- extract_dots(...)
-
-##   ## Handling sequences
-##   seq_p <- vapply(dots, is.seq_gs, logical(1))
-##   if (any(seq_p))
-##     return(do.call("seq_operate", c(dots, list(fun = cbind.data))))
-
-##   names <- vapply(dots, get_name, character(1)) %>%
-##     make_names_unique
-
-##   dots %>%
-##     set_names(names) %>%
-##     convert_numeric %>%
-##     list_gs %>%
-##     ensure_same_length
-## }
-
-
-cbind.gs_seq <- function(...) {
-  stop()
-}
-
-## cbind.posterior <- function(...) {
-##   args <- extract_dots(...)
-##   if (!is.list_gs(args))
-##     args <- concatenate(args)
-
-##   res <- Reduce(cbind, args, NULL)
-##   class(res) <- c("posterior", "matrix_gs")
-##   res
-## }
-
-
-
-rbind.data <- function(...) {
-  dots <- dots(...)
-
-  seq_p <- vapply(dots, is.seq_gs, logical(1))
-  if (any(seq_p))
-    return(do.call("seq_operate", c(dots, list(fun = rbind.data))))
-
-  do.call("c", dots) %>%
-    convert_numeric
-}
-
-
-rbind.posterior <- function(...) {
-  dots <- dots(...)
-
-  seq_p <- vapply(dots, is.seq_gs, logical(1))
-  if (any(seq_p))
-    return(do.call(seq_operate, c(dots, list(fun = rbind.posterior))))
-
-  do.call(cbind, lapply(dots, unclass))
-}
-
-
-rbind_cols <- function(x, ...) UseMethod("rbind_cols")
-
-rbind_cols.data <- function(object) {
+`_rbind_cols` <- function(object) {
   if (!dim_length(object) == 2)
     stop("Can only rbind the cols of a matrix")
 
@@ -70,9 +8,7 @@ rbind_cols.data <- function(object) {
 
   ncols <- ncol(object)
   nrows <- nrow(object)
-  res <- object %>%
-    array(c(nrows * ncols, 1)) %>%
-    gs("data")
+  res <- array(object, c(nrows * ncols, 1))
 
   attr(res, "blocks_names") <- colnames(object) %||% rep("", ncols)
   attr(res, "blocks_indices") <- c(0, cumsum(rep(nrows, ncols)))
@@ -81,20 +17,13 @@ rbind_cols.data <- function(object) {
   res
 }
 
-rbind_cols.matrix <- rbind_cols.data
 
-rbind_cols.posterior <- function(object) {
-  do_by_sims(object, fun = rbind_cols.data) 
-}
-
-cbind_blocks <- function(x) UseMethod("cbind_blocks")
-
-cbind_blocks.data <- function(object) {
+`_cbind_blocks` <- function(object) {
   if (!dim_length(object) == 2)
     stop("Can only cbind the blocks of a matrix or a column vector")
 
   if (dim(object)[2] > 1)
-    browser(expr = getOption("debug_on"))
+    stop("todo")
 
   dims <- dim(object)
   blocks <- attr(object, "blocks_names")
@@ -104,29 +33,4 @@ cbind_blocks.data <- function(object) {
   res <- array(object, c(gap, length(blocks) * last(dims)))
   colnames(res) <- blocks
   res
-}
-
-cbind_blocks.posterior <-
-  function(object) do_by_sims(object, cbind_blocks(.$object))
-
-
-
-as.data.frame.matrix_gs <- function(gs, ...) {
-  if (is.data(gs)) {
-    as.data.frame.matrix(gs, ...)
-  }
-}
-
-
-#' @export
-is.matrix_gs <- function(x) inherits(x, "matrix_gs")
-
-
-as.matrix.list_gs <- function(x) {
-  old_class <- c("gs", gsim_class(x))
-  old_name <- name(x)
-
-  (do.call(cbind, x) %>%
-    set_class(old_class) %>%
-    set_name(old_name))
 }
--- R/operators.r
+++ R/operators.r
@@ -10,6 +10,63 @@ operator <- function(fun) {
   }
 }
 
+operate <- function(..., fun) {
+  dots <- dots(...) %>% lapply(sanitize)
+  all_data <- all(vapply(dots, function(x) gsim_class(x) == "data", logical(1)))
+
+  if (all_data)
+    do.call(fun, dots) %>% gs("data")
+  else
+    do.call(do_by_sims, c(dots, list(fun = fun)))
+}
+
+do_by_sims <- function(..., fun, args = NULL) {
+  ## Normally the enclosing environment gets passed along with the
+  ## promises contained in dots. This sometimes does not happen when
+  ## `do_by_sims` is called directly within a vsim object. The
+  ## following is a workaround for this situation. Also, sometimes
+  ## lazy_dots can not follow symbols and will fail, while usual
+  ## methods of capturing dots work (but do not work in all
+  ## circumstances so still need lazy_dots just in case). Todo: fix
+  ## this mess.
+
+  try_dots <- try({
+    dots <- lazy_dots(..., .follow_symbols = TRUE)
+    env <- input_env()
+    dots <- pluck(dots, "expr") %>% lapply(eval, envir = env)
+    dots
+  }, silent = TRUE)
+  if (class(try_dots) == "try-error")
+    dots <- dots(...)
+
+  dots <- lapply(dots, function(x) {
+    if (is.data(x))
+      list(x %>% set_class("numeric"))
+    else
+      x
+  })
+
+
+  ## Workaround for weird dispatching bug: if fun is a S3 generic, it
+  ## won't find the method. Defining a local copy resolves this. (???)
+  ## See note in vapply help page?
+
+  ## Note: tried to change the parent env of fun to the execution
+  ## env of do_by_sims (`environment(fun) <- environment()`), but it
+  ## didn't work.
+  fnu <- function(...) fun(...)
+
+
+  ## Small perf advantage for lapply in case of unary mappings
+  res <- 
+    if (length(dots) == 1)
+      do.call(lapply, c(list(FUN = fnu), dots, args))
+    else
+      do.call(Map, c(list(f = fnu), dots, args))
+
+  res %>% set_gsim_attributes("posterior")
+}
+
 
 sanitize <- function(object, dim_other) {
   if (is.posterior(object)) {
@@ -26,7 +83,6 @@ sanitize <- function(object, dim_other) {
     return(res)
   }
 
-
   if (length(dim(object)) == 2 && all(dim(object) == c(1, 1)))
     object <- as.vector(object)
   
@@ -41,64 +97,6 @@ sanitize <- function(object, dim_other) {
   class(object) <- setdiff(class(object), "gs")
 
   object
-}
-
-
-operate <- function(..., fun) {
-  dots <- dots(...) %>% lapply(sanitize)
-  all_data <- all(vapply(dots, function(x) gsim_class(x) == "data", logical(1)))
-
-  if (all_data)
-    do.call("fun", dots) %>% gs("data")
-  else
-    do.call("do_by_sims", c(dots, list(fun = fun)))
-}
-
-do_by_sims <- function(..., fun, args = NULL) {
-  ## Normally the enclosing environment gets passed along with the
-  ## promises contained in dots. This sometimes does not happen when
-  ## `do_by_sims` is called directly within a vsim object. The
-  ## following is a workaround for this situation. Also, sometimes
-  ## lazy_dots can not follow symbols and will fail, while usual
-  ## methods of capturing dots work...
-
-  try_dots <- try({
-    dots <- lazy_dots(..., .follow_symbols = TRUE)
-    env <- input_env()
-    dots <- dots %>% pluck("expr") %>% lapply(eval, envir = env)
-    dots
-  }, silent = TRUE)
-
-  if (class(try_dots) == "try-error")
-    dots <- dots(...)
-
-  dots <- lapply(dots, function(x) {
-    if (is.data(x))
-      list(x %>% set_class("numeric"))
-    else
-      x
-  })
-
-
-  ## Workaround for weird dispatching bug: if fun is a S3 generic, it
-  ## won't find the method. Defining a local copycat resolves
-  ## this. (???) See note in vapply help page?
-
-  ## Note: tried to change the enclosing env of fun to the execution
-  ## env of do_by_sims (`environment(fun) <- environment()`), but it
-  ## didn't work.
-  fnu <- function(...) fun(...)
-
-
-  ## Small perf advantage for lapply for unary mappings
-  res <- 
-    if (length(dots) == 1)
-      do.call("lapply", c(list(FUN = fnu), dots, args))
-    else
-      do.call("Map", c(list(f = fnu), dots, args))
-
-  res %>%
-    set_gsim_attributes("posterior")
 }
 
 
@@ -120,6 +118,8 @@ make_beep <- function(fun) {
 `^.gs` <- operator(`^`)
 `cbind.gs` <- operator(cbind)
 `rbind.gs` <- operator(rbind)
+`rbind_cols` <- operator(`_rbind_cols`)
+`cbind_blocks` <- operator(`_cbind_blocks`)
 `%*%.gs` <- operator(`%*%`)
 `subset.gs` <- operator(`[`)
 inv_logit <- operator(ilogit)
